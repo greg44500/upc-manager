0339faeb52eb27afb8bf4828765d6ef29c1e2efe
Gregory BALLAT <gregdevweb44500@gmail.com>
2024-08-15 15:21:18 +0200

    Cors Implementation

--- server.js
+++ server.js
@@ -7,8 +7,18 @@ const cookieParser = require("cookie-parser");
 const userRoutes = require("./routes/user.routes");
 const productRoutes = require("./routes/product.routes.js");
 
+//CORS AND HEADERS REQUEST SECURITY
+const corsOptions = {
+  origin: process.env.CLIENT_URL,
+  credentials: true,
+  allowedHeaders: ["sessionId", "Content-Type"],
+  exposedHeaders: ["sessionId"],
+  methods: "GET, HEAD, PUT, POST,DELETE", // AJOUTER PATCH SI BESOIN
+  preflightContinue: false,
+};
+app.use(cors(corsOptions));
 app = express();
-app.use(cors());
+
 app.use(express.json());
 app.use(cookieParser());
 connectDB(); //Methode de connexion MongoDB
